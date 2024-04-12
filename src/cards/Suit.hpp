8626cf154232c721674ec70225d8ef1536125740
sedwinj <s3782699@student.rmit.edu.au>
2024-04-12 17:49:42 +1000

    Add Suit management logic

---
+++ src/cards/Suit.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+class Suit
+{
+public:
+  enum Color
+  {
+    RED,
+    BLACK
+  };
+
+  Color getColor();
+  std::string getName();
+  std::string toString();
+  static const std::vector<Suit> &getSuits();
+
+private:
+  const std::string name;
+  const Color color;
+  static std::vector<Suit> suits;
+
+  Suit(std::string name, Color color);
+  static const std::vector<Suit> &buildSuits();
+};
