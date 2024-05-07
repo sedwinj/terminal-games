38fe7076cf11946445ad0acc0ea619bdbb87f26c
sedwinj <s3782699@student.rmit.edu.au>
2024-05-07 16:46:32 +1000

    Refactor Suit into Card class and start work on Card

--- src/Card/Card.cpp
+++ src/Card/Card.cpp
@@ -1,45 +1,115 @@
 #include "Card.hpp"
 
-Card::Card(Suit suit, int value) : suit(suit),
-                                   value(value),
-                                   black(suit == CLUBS || suit == SPADES) {}
+Card::Card(int value, Suit suit) : value(value), suit(suit) {}
 
-Suit Card::getSuit()
+Card::Card(int value, std::string suit) : value(value),
+                                          suit(parseSuit(suit)) {}
+
+Card::Card(std::string value, Suit suit) : value(parseValue(value)),
+                                           suit(suit) {}
+
+Card::Card(std::string value, std::string suit) : value(parseValue(value)),
+                                                  suit(parseSuit(suit)) {}
+
+Card::Card(const Card &other) : value(other.value), suit(other.suit) {}
+
+Card::Suit Card::getSuit() const
 {
   return suit;
 }
 
-int Card::getValue()
+int Card::getValue() const
 {
   return value;
 }
 
+std::vector<Card *> Card::newDeck()
+{
+  // TODO: implement
+}
+
+bool Card::operator==(const Card &other) const
+{
+  return value == other.value && suit == other.suit;
+}
+
+bool Card::operator!=(const Card &other) const
+{
+  return !(*this == other);
+}
+
+Card *Card::parse(std::string str)
+{
+  // TODO: implement
+}
+
+Card::Suit Card::parseSuit(std::string suit)
+{
+  // TODO: implement
+}
+
+int Card::parseValue(std::string value)
+{
+  // TODO; implement
+}
+
 std::string Card::toString()
+{
+  return valueToString(value) + " of " + suitToString(suit);
+}
+
+std::string Card::toSymbol()
+{
+  // TODO: implement
+}
+
+std::string Card::suitToString(Suit suit)
+{
+  switch (suit)
+  {
+  case HEARTS:
+    return "Hearts";
+  case CLUBS:
+    return "Clubs";
+  case DIAMONDS:
+    return "Diamonds";
+  case SPADES:
+    return "Spades";
+  }
+}
+
+std::string Card::valueToString(int value)
 {
   std::string valueString;
   switch (value)
   {
   case 1:
-    valueString = "Ace";
-    break;
+    return "Ace";
+  case 2:
+    return "Two";
+  case 3:
+    return "Three";
+  case 4:
+    return "Four";
+  case 5:
+    return "Five";
+  case 6:
+    return "Six";
+  case 7:
+    return "Seven";
+  case 8:
+    return "Eight";
+  case 9:
+    return "Nine";
+  case 10:
+    return "Ten";
   case 11:
-    valueString = "Jack";
-    break;
+    return "Jack";
   case 12:
-    valueString = "Queen";
-    break;
+    return "Queen";
   case 13:
-    valueString = "King";
+    return "King";
   default:
-    valueString = std::to_string(value);
+    return std::to_string(value);
   }
-
-  std::string suitString;
-  switch (suit)
-  {
-  case HEARTS:
-    suitString = ""
-  }
-
-  return
 }
--- src/Card/Card.hpp
+++ src/Card/Card.hpp
@@ -1,15 +1,64 @@
+#include <memory>
 #include <string>
+#include <vector>
 
 class Card
 {
 public:
-  Card(Suit suit, int value);
-  Suit getSuit();
-  int getValue();
+  enum Suit
+  {
+    HEARTS,
+    CLUBS,
+    DIAMONDS,
+    SPADES
+  };
+
+  enum Color
+  {
+    RED,
+    BLACK
+  };
+
+  static std::vector<Card *> newDeck();
+
+  /*  Parses a string and converts it to a card. Accepted formats are of the
+   *  form:
+   *  - <charvalue><charnumber> (e.g.: "QH", "12H")
+   *  - <charvalue> <charnumber> (e.g.: "Q H", "12 H")
+   *  - <value> of <number> (e.g.: "queen of hearts", "12 of hearts",
+   *    "twelve of hearts")
+   */
+  static Card *parse(std::string str);
+
+  // Constructors
+  Card(int value, Suit suit);
+  Card(int value, std::string suit);
+  Card(std::string value, Suit suit);
+  Card(std::string value, std::string suit);
+
+  // Copy constructor
+  Card(const Card &other);
+
+  Suit getSuit() const;
+  int getValue() const;
+  bool operator==(const Card &other) const;
+  bool operator!=(const Card &other) const;
   std::string toString();
+  std::string toSymbol();
 
 private:
-  const Suit suit;
-  const int value;
-  const bool black;
+  enum Type
+  {
+    CHAR = 0,
+    STR = 1,
+    SYMBOL = 2
+  };
+
+  Suit suit;
+  int value;
+
+  static Suit parseSuit(std::string suit);
+  static int parseValue(std::string value);
+  static std::string suitToString(Suit suit);
+  static std::string valueToString(int value);
 };
