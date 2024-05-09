97ca1ba45c8555dbf67a985479f3679e1e0b0468
sedwinj <s3782699@student.rmit.edu.au>
2024-05-09 14:26:32 +1000

    Add useful useful string methods to Util

--- src/Util.hpp
+++ src/Util.hpp
@@ -1,15 +1,18 @@
+#pragma once
+
 #include <string>
 #include <vector>
 
 namespace Util
 {
-  // Returns true if the value is in the array, and false otherwise.
   template <typename T>
   bool contains(const std::vector<T> &array, const T &value);
+  inline std::string stringLower(const std::string &in);
+  inline std::vector<std::string> stringSplit(const std::string &str);
+  inline std::vector<std::string> stringSplit(const std::string &str, const std::string &delim);
+  inline std::string stringTrim(const std::string &str);
 
-  // Returns a copy of the input without leading and trailing whitespace characters.
-  std::string stringTrim(const std::string &str);
-
+  // Returns true if the value is in the array, and false otherwise.
   template <typename T>
   inline bool contains(const std::vector<T> &array, const T &value)
   {
@@ -24,6 +27,44 @@ namespace Util
     return false;
   }
 
+  // Returns a copy of the string with all letters converted to lowercase.
+  inline std::string stringLower(const std::string &in)
+  {
+    std::string out = in;
+    for (int idx = 0; idx < out.size(); idx++)
+    {
+      if ('A' <= out[idx] && out[idx] <= 'Z')
+      {
+        out[idx] += 'a' - 'A';
+      }
+    }
+    return out;
+  }
+
+  // Splits the string, using commas as a delimiter.
+  inline std::vector<std::string> stringSplit(const std::string &str)
+  {
+    return stringSplit(str, ",");
+  }
+
+  // Splits the string using the specified delimiter.
+  inline std::vector<std::string> stringSplit(const std::string &str, const std::string &delim)
+  {
+    std::vector<std::string> split;
+    int left = 0;
+    int right = str.find(delim, left);
+    while (right != std::string::npos)
+    {
+      split.push_back(str.substr(left, right - left));
+      left = right + delim.size();
+      right = str.find(delim, left);
+    }
+    split.push_back(str.substr(left));
+
+    return split;
+  }
+
+  // Returns a copy of the input without leading and trailing whitespace characters.
   inline std::string stringTrim(const std::string &str)
   {
     // TODO: test
--- test/Util.test.cpp
+++ test/Util.test.cpp
@@ -22,8 +22,60 @@ TEST_CASE("contains", "[Util]")
   REQUIRE(!Util::contains<int>(numbers, 6));
 }
 
+TEST_CASE("stringLower", "[Util]")
+{
+  std::string word = "WoRd";
+  REQUIRE(Util::stringLower(word) == "word");
+}
+
+TEST_CASE("stringSplit", "[Util]")
+{
+  std::string list = "Alpha,Beta,Gamma,Delta,Epsilon";
+  std::vector<std::string> expected1;
+  expected1.push_back("Alpha");
+  expected1.push_back("Beta");
+  expected1.push_back("Gamma");
+  expected1.push_back("Delta");
+  expected1.push_back("Epsilon");
+  auto actual1 = Util::stringSplit(list);
+  REQUIRE(expected1 == actual1);
+
+  std::string phrase = "Lorem ipsum dolor sit amet";
+  std::vector<std::string> expected2;
+  expected2.push_back("Lorem");
+  expected2.push_back("ipsum");
+  expected2.push_back("dolor");
+  expected2.push_back("sit");
+  expected2.push_back("amet");
+  auto actual2 = Util::stringSplit(phrase, " ");
+  REQUIRE(expected2 == actual2);
+
+  std::string numbers = "one, two, three, four, five";
+  std::vector<std::string> expected3;
+  expected3.push_back("one");
+  expected3.push_back("two");
+  expected3.push_back("three");
+  expected3.push_back("four");
+  expected3.push_back("five");
+  auto actual3 = Util::stringSplit(numbers, ", ");
+  REQUIRE(expected3 == actual3);
+
+  std::string empties = ",hello,,world,";
+  std::vector<std::string> expected4;
+  expected4.push_back("");
+  expected4.push_back("hello");
+  expected4.push_back("");
+  expected4.push_back("world");
+  expected4.push_back("");
+  auto actual4 = Util::stringSplit(empties);
+  REQUIRE(expected4 == actual4);
+}
+
 TEST_CASE("stringTrim", "[Util]")
 {
   std::string whitespaceWord = " \n\tword\t\n ";
   REQUIRE(Util::stringTrim(whitespaceWord) == "word");
+
+  std::string phrase = "middle space";
+  REQUIRE(Util::stringTrim(phrase) == "middle space");
 }
