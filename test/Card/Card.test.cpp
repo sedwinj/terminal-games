456cf8939c8408d4dd8d0ff0370bf7b523602e7d
sedwinj <s3782699@student.rmit.edu.au>
2024-05-09 14:27:34 +1000

    Add more comprehensive tests for card constructors

--- test/Card/Card.test.cpp
+++ test/Card/Card.test.cpp
@@ -4,12 +4,19 @@
 
 #include "src/Card/Card.hpp"
 
-TEST_CASE("Persistence", "[Card::Card]")
+TEST_CASE("Constructors", "[Card::Card]")
 {
-  Card *card = new Card(12, Card::Suit::HEARTS);
+  Card *card1 = new Card(12, Card::Suit::HEARTS);
+  Card *card2 = new Card(12, "hearts");
+  Card *card3 = new Card("twelve", Card::Suit::HEARTS);
+  Card *card4 = new Card("twelve", "hearts");
+  Card *card5 = new Card(*card4);
 
-  REQUIRE(card->getValue() == 12);
-  REQUIRE(card->getSuit() == Card::Suit::HEARTS);
+  REQUIRE((card1->getValue() == 12 && card1->getSuit() == Card::Suit::HEARTS));
+  REQUIRE((card2->getValue() == 12 && card2->getSuit() == Card::Suit::HEARTS));
+  REQUIRE((card3->getValue() == 12 && card3->getSuit() == Card::Suit::HEARTS));
+  REQUIRE((card4->getValue() == 12 && card4->getSuit() == Card::Suit::HEARTS));
+  REQUIRE((card5->getValue() == 12 && card5->getSuit() == Card::Suit::HEARTS));
 }
 
 TEST_CASE("Equality", "[Card]::==")
